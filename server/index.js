44f8846e6758b20a266891b3ed4f206f0e656593
Ramones Fuentes <donjoaquin629@gmail.com>
2024-10-12 10:25:00 +0800

    Todos Routes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 import express from "express"
+import todosRouter from "./routes/todos.js";
 const app = express()
 
-const port = 3000;
+const port = 4000;
 
 app.get("/", (req, res) => {
-    res.send("Hello hello ka jan!!!");
+    res.send("Hello Todo hello ka jan!!!");
   });
 
   app.listen(port, () => {
     console.log(`Listening to port ${port}`);
   });
+  app.use("/todo",todosRouter)
---
+++ server/routes/todos.js
@@ -0,0 +1,38 @@
+import express from "express";
+const todosRouter = express.Router();
+
+
+// GET all todos
+todosRouter.get("/todos", (req, res) => {
+  res.send(`All Todos`);
+});
+
+// GET a specific todo by id
+todosRouter.get("/todos/:id", (req, res) => {
+  const todoId = req.params.id;
+  res.send(`Getting TODO with id ${todoId}`);
+});
+
+// POST to create a new todo
+todosRouter.post("/todos", (req, res) => {
+  const   newTodo = {
+    id: 1,
+    title: "Todo",
+    description: "My todo",
+  };
+  res.json(newTodo);
+});
+
+// PUT to update a todo by id
+todosRouter.put("/todos/:id", (req, res) => {
+  const todoId = req.params.id;
+  res.send(`Updating TODO with id ${todoId}`);
+});
+
+// DELETE a specific todo by id
+todosRouter.delete("/todos/:id", (req, res) => {
+  const todoId = req.params.id;
+  res.send(`Deleting TODO with id ${todoId}`);
+});
+
+export default todosRouter;
